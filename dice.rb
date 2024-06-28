0035d89a35a11321310ef45e1dced534804191f2
SRISAI BODDULURI <32993841+boddulurisrisai@users.noreply.github.com>
2024-06-28 18:03:59 +0000

    dice updated

--- dice.rb
+++ dice.rb
@@ -17,52 +17,63 @@ end
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
+  @rolls=[]
+    @rolls.push(first_die)
+    @rolls.push(second_die)
+  '''
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+'''
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1...10)
-  sum = first_die + second_die
-  outcome = "You rolled #{first_die} and #{second_die} and there sum is #{sum}"
+  '''sum = first_die + second_die
+  outcome = "You rolled #{first_die} and #{second_die} and there sum is #{sum}"'''
+  @rolls = [first_die, second_die]
+  erb(:two_ten)
 
-  "<h1> 2d10 </h12>
-  <p> #{outcome}</p>"
+
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @die = rand(1..20)
   
-  outcome = "You rolled #{first_die}"
-
-  "<h1> 1d20 </h12>
-  <p> #{first_die}</p>"
+  @outcome = "#{@die}"
+  erb(:one_twenty)
+  
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
+  '''first_die = rand(1..4)
   second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
-  sum1 = first_die + second_die + thid_die + fourth_die
+  sum1 = first_die + second_die + third_die + fourth_die
   
-  outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, and #{fourth_die}"
-
-  "<h1> 5d4 </h12>
-  <p> #{outcome}</p>"
+  outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, and #{fourth_die}"'''
+@array=[]
+  4.times do
+    @array.push(rand(1..4))
+  end
+erb(:five_four)
+  
 end
 
 get("/") do
-"<h1> Dice ROll</h1>
-<ul>
-<li> <a href ='/dice/2/10'> Roll two 10 sided dice</a></li>
-<li> <a href ='/dice/2/10'> Roll two 6 sided dice</a></li>
-<li> <a href ='/dice/1/20'> Roll one 20 sided dice</a></li>
-<li> <a href ='/dice/5/4'> Roll five 4 sided dice</a></li>
-</ul>"
+  erb(:elepanth)
+end
+
+get("/dice/100/6") do
+  @rolls = []    
+
+  100.times do    
+    die = rand(1..6)    
+
+    @rolls.push(die)    
+  end
+
+  erb(:one_hundred_six)
 end
